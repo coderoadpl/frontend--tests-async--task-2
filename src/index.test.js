48288a1aada69aa74302ff2d336544ba626d5777
Mateusz Choma <kontakt@coderoad.pl>
2021-04-07 17:45:23 +0200

    Testing callbacks with `done`

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,14 @@
-it('should pass', () => {
-    expect(true).toBe(true)
+// if we need more time to execute, default is 5 000 ms
+// jest.setTimeout(10000)
+
+it('should pass with callback', (done) => {
+    expect.assertions(1)
+
+    setTimeout(
+        () => {
+            expect(true).toBe(true)
+            done()
+        },
+        0
+    )
 })
